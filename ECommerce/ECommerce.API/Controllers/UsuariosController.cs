10f72bcf5b5e5fd96389ada0979aa2bba0747adb
Alanp <Alanp@ALAN>
2025-04-15 19:32:18 -0300

    criaçao da controller de usuários

---
+++ ECommerce/ECommerce.API/Controllers/UsuariosController.cs
@@ -0,0 +1,57 @@
+﻿using ECommerce.API.Repositories;
+using ECommerce.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly IUsuarioRepository _repository;
+
+        public UsuariosController(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var lisaUsuarios = _repository.Get();
+            return Ok(lisaUsuarios);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var usuario = _repository.Get(id);
+            if (usuario == null)
+                return NotFound("Não encontrado");
+
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody]Usuario usuario)
+        {
+            _repository.Add(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody] Usuario usuario) 
+        { 
+            _repository.Update(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
+    }
+}
